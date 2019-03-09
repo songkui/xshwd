1270d1112597d02fe9cf90a9475ffb6f9aa315b8
songkui <415937387@qq.com>
2019-03-09 11:50:42 +0800

    add case

---
+++ xshwd-order/src/main/java/com/xshwd/order/config/mybatis/MyMetaObjectHandler.java
@@ -0,0 +1,43 @@
+/*
+ *  THIS FILE IS PART OF C8software PROJECT
+ * Copyright (c) 2011 - 2018 C8.Co.Ltd. All rights reserved.
+ * Mr.Yellow (www.c8software.com) 18-1-16 下午10:46
+ *
+ *
+ */
+
+package com.xshwd.item.config.mybatis;
+
+import com.baomidou.mybatisplus.mapper.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+
+import java.util.Date;
+
+public class MyMetaObjectHandler extends MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+
+
+        super.setFieldValByName("createTime",new Date(),metaObject);
+
+
+        super.setFieldValByName("status",1,metaObject);
+
+
+        super.setFieldValByName("version",1,metaObject);
+
+
+    }
+
+
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+        super.setFieldValByName("updateTime",new Date(),metaObject);
+
+
+
+
+    }
+}
---
+++ xshwd-order/src/main/java/com/xshwd/order/service/FeginTestService.java
@@ -0,0 +1,50 @@
+package com.xshwd.item.service;
+
+//import com.xshwd.item.config.redis.RedisServer;
+import com.xshwd.item.fegin.UserFeginService;
+import com.xshwd.orm.user.entity.AccWxUser;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Auther: SK ON  2018/10/29 14:58
+ * @Description:
+ */
+
+@Service
+public class FeginTestService {
+
+    @Autowired
+    private UserFeginService userFeginService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 通过openId查询
+     * @param openId
+     * @return
+     */
+    public AccWxUser getUserByOpenId(String openId){
+        try{
+            System.out.println("----~~~~~~~~~~~~~~--------------");
+            stringRedisTemplate.opsForValue().set("AASKAA","123");
+            stringRedisTemplate.expire("AASKAA", 5, TimeUnit.MINUTES);
+            System.out.println("-----~~~~~~~~~~-------------");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return userFeginService.getByshopId(openId).getData();
+    }
+
+
+
+
+
+}
